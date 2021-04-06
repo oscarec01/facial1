7ec3b8f1dba215c7ce6d3d84d5e29f081b266341
Oscar Gualteros <61791172+oscarec01@users.noreply.github.com>
2021-04-06 14:34:39 -0500

    primer template

--- aplicaciones/principal/views.py
+++ aplicaciones/principal/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from .models import Persona, Usuarios, Tiposdni, Estado
 
-# Create your views here.
+def inicio(request):
+    return render(request, 'index.html')
