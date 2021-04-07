d8b197c6c5dadac0c5f21efaff63265e35efd507
Oscar Gualteros <61791172+oscarec01@users.noreply.github.com>
2021-04-07 08:57:33 -0500

    Update views.py

--- aplicaciones/principal/views.py
+++ aplicaciones/principal/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Persona, Usuarios, Tiposdni, Estado
-from .forms import UsuarioForm
+from .forms import UsuarioForm, PersonaForm
 
 def inicio(request):
     return render(request, 'index.html')
@@ -27,3 +27,22 @@ def editar_usuario(request, id):
     else:
         formaUsuario = UsuarioForm(instance=usuario)
     return render(request, 'editar_usuario.html', {'formaUsuario': formaUsuario})
+
+
+def crearPersona(request):
+    if request.method == 'GET':
+        form = PersonaForm()
+        contexto={
+            'form':form
+        }
+    else:
+        form = PersonaForm(request.POST)
+        form = PersonaForm()
+        contexto={
+            'form':form
+        }
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+            
+    return render(request, 'crear_persona.html',contexto)
